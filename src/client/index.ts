5d6b8eaf2768a51bb37e7b0d195786f268a3ea17
François Marie De Mey <eeddow@gmail.com>
2024-06-23 13:47:25 +0300

    1.1.17

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,4 +2,9 @@ export { default as I18nClient } from './client'
 export * from './client'
 export { TranslationError, type ClientDictionary, type Translator } from './types'
 export { bulkObject, bulkDictionary } from './helpers'
-export { formats, processors, DurationDescription, DurationOptions } from './interpolation'
+export {
+	formats,
+	processors,
+	type DurationDescription,
+	type DurationOptions
+} from './interpolation'
